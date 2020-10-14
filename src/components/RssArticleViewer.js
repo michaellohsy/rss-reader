fec8484a80c93ec2454149e994a3888674b51252
michaellohsy <michaellohsy@gmail.com>
2020-10-14 22:22:54 +0800

    Added pagination

--- src/components/RssArticleViewer.js
+++ src/components/RssArticleViewer.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 import { Col, Form, Pagination, Row } from 'react-bootstrap';
 import RssArticle from './RssArticle';
 
+const pageSizeSelectStyle = {
+  display: "flex",
+  alignItems: "center",
+};
+
+const pageText = {
+  textAlign: "center",
+};
+
 class RssArticleViewer extends Component {
   constructor(props) {
     super(props)
     this.state = {
        pageSize: 10,
        page: 1,
-       activePage: 1,
        articles: [],
     }
     this.pageSizes = [10, 25, 50, 100];
@@ -33,7 +41,7 @@ class RssArticleViewer extends Component {
       this.props.articles.length / this.state.pageSize;
     for (let i = 1; i <= totalPages; i++) {
       items.push(
-        <Pagination.Item key={i} active={i === this.state.activePage} onClick={() => this.onPageChanged(i)}>
+        <Pagination.Item key={i} active={i === this.state.page} onClick={() => this.onPageChanged(i)}>
           {i}
         </Pagination.Item>,
       );
@@ -43,19 +51,28 @@ class RssArticleViewer extends Component {
 
   render() {
     const pageArticles = this.props.articles.slice((this.state.page - 1) * this.state.pageSize, this.state.page * this.state.pageSize);
+    const firstIdx = (this.state.page - 1) * this.state.pageSize + 1;
+    const lastIdx = (this.state.page - 1) * this.state.pageSize + this.state.pageSize;
     return (
       <div>
         {pageArticles.map(article =>
           (<RssArticle key={article.guid} article={article}/>)
         )}
         <Row>
-          <Col md={8}>
+          <Col md={4}>
             <Pagination>{this.getPagination()}</Pagination>
           </Col>
           <Col md={4}>
-            <Form.Control inline as="select" value={this.state.pageSize} onChange={this.onPageSizeChange} >
-              {this.pageSizes.map(p => (<option>{p}</option>))}
-            </Form.Control>
+            <div style={pageText}>{`Viewing ${firstIdx} - ${lastIdx} of ${this.props.articles.length}`}</div>
+          </Col>
+          <Col md={4}>
+            <div style={pageSizeSelectStyle}>
+              <span>Show&nbsp;&nbsp;</span>
+              <Form.Control as="select" value={this.state.pageSize} onChange={this.onPageSizeChange} >
+                {this.pageSizes.map(p => (<option>{p}</option>))}
+              </Form.Control>
+              <span>&nbsp;&nbsp;Feeds</span>
+            </div>
           </Col>
         </Row>
       </div>
