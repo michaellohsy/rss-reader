74f586ec2f4e3dc1d9b84f922c2c135ff14d9945
michaellohsy <michaellohsy@gmail.com>
2020-10-19 23:18:50 +0800

    Fixed more warnings

--- src/components/RssFeeds.js
+++ src/components/RssFeeds.js
@@ -55,7 +55,7 @@ class RssFeeds extends Component {
   }
 
   isRssLink(string) {
-    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const expression = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;
     const regex = new RegExp(expression);
     return string.match(regex);
   }
