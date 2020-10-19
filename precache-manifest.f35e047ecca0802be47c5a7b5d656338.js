10ab4520b73774f26ec2ee7d44fe9510ebffc286
michaellohsy <michaellohsy@users.noreply.github.com>
2020-10-19 15:20:57 +0000

    deploy: 74f586ec2f4e3dc1d9b84f922c2c135ff14d9945

---
+++ precache-manifest.f35e047ecca0802be47c5a7b5d656338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54791366b0b9eb3bfd066e096c1b6b29",
+    "url": "/rss-reader/index.html"
+  },
+  {
+    "revision": "2a9899a927768ce895dc",
+    "url": "/rss-reader/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0b30ebc7d4589c412888",
+    "url": "/rss-reader/static/css/main.392203cb.chunk.css"
+  },
+  {
+    "revision": "2a9899a927768ce895dc",
+    "url": "/rss-reader/static/js/2.d89928f2.chunk.js"
+  },
+  {
+    "revision": "594f8b3fb1fe0b6a3d9abea13b31ee07",
+    "url": "/rss-reader/static/js/2.d89928f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b30ebc7d4589c412888",
+    "url": "/rss-reader/static/js/main.af8bfd86.chunk.js"
+  },
+  {
+    "revision": "6d1fd6a9fc9907ee9d7f",
+    "url": "/rss-reader/static/js/runtime-main.20c6be00.js"
+  }
+]);
